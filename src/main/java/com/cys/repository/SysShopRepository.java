cb0a258ff88bfefaed20013e2463aea81204e2cc
tangxing3596 <410289623@qq.com>
2018-03-04 23:09:18 +0800

    新增店铺dao类

---
+++ src/main/java/com/cys/repository/SysShopRepository.java
@@ -0,0 +1,18 @@
+package com.cys.repository;
+
+import com.cys.model.SysShop;
+import com.cys.model.SysUser;
+
+import java.util.List;
+
+/**
+ * Created by liyuan on 2018/1/28.
+ */
+public interface SysShopRepository extends BaseJpaRepository<SysShop,String> {
+
+    SysUser findByAccount(String account);
+
+    List<SysShop>  findAll();
+
+    List<SysUser> findByShopId(String shopId);
+}
