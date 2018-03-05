139b28f4eee4fce4aa808e65ec35c08bfac16e01
liyuan2018 <18620945130@163.com>
2018-03-05 20:51:45 +0800

    【BUG】用户关联bug

--- src/main/java/com/cys/service/impl/SysUserRelServiceImpl.java
+++ src/main/java/com/cys/service/impl/SysUserRelServiceImpl.java
@@ -2,9 +2,11 @@ package com.cys.service.impl;
 
 import com.cys.model.SysUserRel;
 import com.cys.service.ISysUserRelService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by liyuan on 2018/3/1.
  */
+@Service("sysUserRelService")
 public class SysUserRelServiceImpl extends BaseRelationServiceImpl<SysUserRel,String> implements ISysUserRelService {
 }
